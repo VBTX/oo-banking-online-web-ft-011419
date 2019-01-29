004be4bb568e4b3bd5e19ba8ef41bcb2bcecb1ad
Victoria B <viktorija.bourassa@gmail.com>
2019-01-29 20:59:37 +0000

    Done.

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -13,17 +13,20 @@ class Transfer
   end
 
 
-
   def execute_transaction
     if valid? && @status =="pending"
+      if amount < @sender.balance
       @sender.balance = @sender.balance - @amount
       @receiver.balance = @receiver.deposit(@amount)
       @status = "complete"
       else
         @status = "rejected"
         "Transaction rejected. Please check your account balance."
+      end
     end
   end
+
+
   def reverse_transfer
     if @status == "complete"
       @sender.balance += @amount
